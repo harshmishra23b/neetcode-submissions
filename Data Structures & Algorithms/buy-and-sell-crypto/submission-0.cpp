45ba3b87d565349e6a3b23b9cbe9044e938d7634
Harsh Mishra <harsh.mishra23b@iiitg.ac.in>
2026-08-01 01:41:57 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int mini = prices[0];
+        for(int i=0;i<prices.size();i++){
+            int cost = prices[i];
+            mini = min(cost,mini); 
+            profit = max(profit,prices[i]-mini);
+        }
+        return profit;
+    }
+};
