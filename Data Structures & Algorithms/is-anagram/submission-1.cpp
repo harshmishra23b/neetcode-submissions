0d4a7b85e98d37d432cedaf2a74950c558951d89
Harsh Mishra <harsh.mishra23b@iiitg.ac.in>
2026-07-31 13:24:07 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+   public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        for(int i=0;i<s.size();i++){
+            if(s[i] == t[i]) continue;
+            else return false;
+        }
+
+        return true;
+    }
+};
